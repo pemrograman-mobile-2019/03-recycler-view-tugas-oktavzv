f22baf7bed4993f01b8f97a69bc487462da01c83
Oktavian Yudha <pratama.oktavian66@gmail.com>
2019-02-27 19:51:02 +0700

    First

---
+++ app/src/main/java/com/oktavianyudha/recycleview_tugas/MainActivity.java
@@ -0,0 +1,13 @@
+package com.oktavianyudha.recycleview_tugas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

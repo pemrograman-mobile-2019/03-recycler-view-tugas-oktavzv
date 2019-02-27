2dd7e46d8b76511d58ead1e08334e2d2fea2837e
Oktavian Yudha <pratama.oktavian66@gmail.com>
2019-02-28 03:19:56 +0700

    RecycleView - Tugas (Final)

--- app/src/main/java/com/oktavianyudha/recycleview_tugas/MainActivity.java
+++ app/src/main/java/com/oktavianyudha/recycleview_tugas/MainActivity.java
@@ -1,37 +1,70 @@
 package com.oktavianyudha.recycleview_tugas;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 
 import com.oktavianyudha.recycleview_tugas.adapter.MyAdapter;
 
 import java.util.ArrayList;
-import java.util.List;
 
-public class MainActivity extends Activity {
-    private RecyclerView recyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager layoutManager;
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private ArrayList<String> mImages = new ArrayList<>();
+    private ArrayList<Integer> mHeader = new ArrayList<Integer>();
+    private ArrayList<Integer> mFooter = new ArrayList<Integer>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
 
-        recyclerView.setHasFixedSize(true);
+        Log.d(TAG, "onCreate: started.");
 
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-        List<String> input = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            input.add("Wallpaper" + i);
-
-            mAdapter = new MyAdapter(input);
-            recyclerView.setAdapter(mAdapter);
-        }
+        initJob();
     }
+
+    private void initJob(){
+        for (int i = 0; i < 50; i++) {
+            mImages.add(String.valueOf(R.id.image1));
+            mHeader.add(R.id.firstLine);
+            mFooter.add(R.id.secondLine);
+        }
+        initRecyclerview();
+    }
+
+     private void initRecyclerview(){
+        RecyclerView rv = (RecyclerView) findViewById(R.id.my_recycler_view);
+        MyAdapter adapter = new MyAdapter(this, mImages,mHeader,mFooter);
+        rv.setAdapter(adapter);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+//    private RecyclerView recyclerView;
+//    private RecyclerView.Adapter mAdapter;
+//    private RecyclerView.LayoutManager layoutManager;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+//
+//        recyclerView.setHasFixedSize(true);
+//
+//        layoutManager = new LinearLayoutManager(this);
+//        recyclerView.setLayoutManager(layoutManager);
+//        List<String> input = new ArrayList<>();
+//        for (int i = 0; i < 50; i++) {
+//            input.add("Wallpaper" + i);
+//
+//            mAdapter = new MyAdapter(input);
+//            recyclerView.setAdapter(mAdapter);
+//        }
+//    }
 }
--- app/src/main/java/com/oktavianyudha/recycleview_tugas/adapter/MyAdapter.java
+++ app/src/main/java/com/oktavianyudha/recycleview_tugas/adapter/MyAdapter.java
@@ -1,82 +1,144 @@
 package com.oktavianyudha.recycleview_tugas.adapter;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.oktavianyudha.recycleview_tugas.R;
 
-import java.util.List;
+import java.util.ArrayList;
+
+import static com.oktavianyudha.recycleview_tugas.R.id.image1;
+
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
-    private List<String> values;
 
+    private static final String TAG = "MyAdapter";
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    private ArrayList<String> mImage = new ArrayList<>();
+    private ArrayList<Integer> mHeaders = new ArrayList<>();
+    private ArrayList<Integer> mFooters = new ArrayList<>();
+    private Context mContext;
 
-        public TextView txtHeader;
-        public TextView txtFooter;
-        public View layout;
-
-        public ViewHolder(View v) {
-            super(v);
-            layout = v;
-            txtHeader = (TextView) v.findViewById(R.id.firstLine);
-            txtFooter = (TextView) v.findViewById(R.id.secondLine);
-        }
+    public MyAdapter(Context mContext, ArrayList<String> mImage, ArrayList<Integer> mHeaders, ArrayList<Integer> mFooters) {
+        this.mImage = mImage;
+        this.mHeaders = mHeaders;
+        this.mFooters = mFooters;
+        this.mContext = mContext;
     }
 
-    public void add(int position, String item) {
-        values.add(position, item);
-        notifyItemInserted(position);
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+        View view = inflater.inflate(R.layout.row_layout, viewGroup ,false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
     }
-
-    public void remove(int position) {
-        values.remove(position);
-        notifyItemRemoved(position);
-    }
-
-
-    public MyAdapter(List<String> myDataset) {
-        values = myDataset;
-    }
-
 
     @Override
-    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
-                                                   int viewType) {
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder,int i) {
+        Log.d(TAG, "onBindViewHolder: called.");
 
-        LayoutInflater inflater = LayoutInflater.from(
-                parent.getContext());
-        View v =
-                inflater.inflate(R.layout.row_layout, parent, false);
-
-        ViewHolder vh = new ViewHolder(v);
-        return vh;
+       viewHolder.imagee.getId();
+       viewHolder.textHeader.setText("Nama");
+       viewHolder.textFooter.setText("Deskripsi");
     }
-
-
-    @Override
-    public void onBindViewHolder(ViewHolder holder, final int position) {
-
-        final String name = values.get(position);
-        holder.txtHeader.setText(name);
-        holder.txtHeader.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                remove(position);
-            }
-        });
-
-        holder.txtFooter.setText("Descriptions: " + name);
-    }
-
 
     @Override
     public int getItemCount() {
-        return values.size();
+        return mImage.size();
     }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+            ImageView imagee;
+            TextView textHeader;
+            TextView textFooter;
+            RelativeLayout parentLayout;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imagee = itemView.findViewById(image1);
+            textHeader = itemView.findViewById(R.id.firstLine);
+            textFooter = itemView.findViewById(R.id.secondLine);
+            parentLayout = itemView.findViewById(R.id.parent_layout);
+        }
+    }
+
+//    private List<String> values;
+//
+//
+//    public class ViewHolder extends RecyclerView.ViewHolder {
+//
+//        public T
+//        public
+//        public View layout;
+//
+//        public ViewHolder(View v) {
+//            super(v);
+//            layout = v;
+//            txtHeader = (TextView) v.findViewById(R.id.firstLine);
+//            txtFooter = (TextView) v.findViewById(R.id.secondLine);
+//        }
+//    }
+//
+//    public void add(int position, String item) {
+//        values.add(position, item);
+//        notifyItemInserted(position);
+//    }
+//
+//    public void remove(int position) {
+//        values.remove(position);
+//        notifyItemRemoved(position);
+//    }
+//
+//
+//    public MyAdapter(List<String> myDataset) {
+//        values = myDataset;
+//    }
+//
+//
+//    @Override
+//    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//
+//        LayoutInflater inflater = LayoutInflater.from(
+//                parent.getContext());
+//        View v =
+//                inflater.inflate(R.layout.row_layout, parent, false);
+//
+//        ViewHolder vh = new ViewHolder(v);
+//        return vh;
+//    }
+//
+//
+//    @Override
+//    public void onBindViewHolder(ViewHolder holder, final int position) {
+//
+//        final String name = values.get(position);
+//        holder.txtHeader.setText(name);
+//        holder.txtHeader.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                remove(position);
+//            }
+//        });
+//
+//        holder.txtFooter.setText("Descriptions: " + name);
+//    }
+//
+//
+//    @Override
+//    public int getItemCount() {
+//        return values.size();
+//    }
 
 }

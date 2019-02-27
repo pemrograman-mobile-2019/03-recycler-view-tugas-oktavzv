b04596d87a7627b2ca9fa54c5bd387a58a336ec9
Oktavian Yudha <pratama.oktavian66@gmail.com>
2019-02-27 22:32:18 +0700

    RecycleView - Tugas (initial 2)

--- app/src/main/java/com/oktavianyudha/recycleview_tugas/MainActivity.java
+++ app/src/main/java/com/oktavianyudha/recycleview_tugas/MainActivity.java
@@ -1,27 +1,36 @@
 package com.oktavianyudha.recycleview_tugas;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+
+import com.oktavianyudha.recycleview_tugas.adapter.MyAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
 
-        RecyclerView rvContacts = (RecyclerView) findViewById(R.id.rvContacts);
+        recyclerView.setHasFixedSize(true);
 
-        // Initialize contacts
-        contacts = Contact.createContactsList(20);
-        // Create adapter passing in the sample user data
-        ContactsAdapter adapter = new ContactsAdapter(contacts);
-        // Attach the adapter to the recyclerview to populate items
-        rvContacts.setAdapter(adapter);
-        // Set layout manager to position the items
-        rvContacts.setLayoutManager(new LinearLayoutManager(this));
-        // That's all!
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        List<String> input = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            input.add("Test" + i);
+
+        mAdapter = new MyAdapter(input);
+        recyclerView.setAdapter(mAdapter);
     }
 }

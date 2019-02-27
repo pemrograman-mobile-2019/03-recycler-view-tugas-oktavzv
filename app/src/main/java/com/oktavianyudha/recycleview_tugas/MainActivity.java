0569a4f8883dbcf36b5ac5b48ef2b51e72ee1cd4
Oktavian Yudha <pratama.oktavian66@gmail.com>
2019-02-27 20:11:22 +0700

    RecycleView - Tugas (initial 1)

--- app/src/main/java/com/oktavianyudha/recycleview_tugas/MainActivity.java
+++ app/src/main/java/com/oktavianyudha/recycleview_tugas/MainActivity.java
@@ -2,6 +2,8 @@ package com.oktavianyudha.recycleview_tugas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView rvContacts = (RecyclerView) findViewById(R.id.rvContacts);
+
+        // Initialize contacts
+        contacts = Contact.createContactsList(20);
+        // Create adapter passing in the sample user data
+        ContactsAdapter adapter = new ContactsAdapter(contacts);
+        // Attach the adapter to the recyclerview to populate items
+        rvContacts.setAdapter(adapter);
+        // Set layout manager to position the items
+        rvContacts.setLayoutManager(new LinearLayoutManager(this));
+        // That's all!
     }
 }
